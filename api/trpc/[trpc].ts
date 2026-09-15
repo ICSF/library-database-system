f870046354dc5eb817b15ce42bd74b7e52f4a80f
Shriya Sistla <ss5324@ic.ac.uk>
2026-09-15 14:29:58 +0100

    remove vercel.ts and directly make a http handler in api/trpc/[trpc.ts]

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,1 +1,7 @@
-export {default} from '../../packages/api/vercel.ts';
+import {appRouter} from '@library/api';
+import {createHTTPHandler} from '@trpc/server/adapters/standalone';
+
+export default createHTTPHandler({
+  router: appRouter,
+  basePath: '/api/trpc/',
+});
--- packages/api/vercel.ts
+++
@@ -1,9 +0,0 @@
-// serverless setup for vercel
-import {createHTTPHandler} from '@trpc/server/adapters/standalone';
-
-import {appRouter} from './index.js';
-
-export default createHTTPHandler({
-  router: appRouter,
-  basePath: '/api/trpc/',
-});
