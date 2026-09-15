1a70eb991c0e120cb8a3d8947a0485f21a9cf6f4
Shriya Sistla <ss5324@ic.ac.uk>
2026-09-15 15:06:42 +0100

    try changing export and import file exts to .js

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,7 +1,7 @@
 import {env} from '@library/config';
 import {PrismaPg} from '@prisma/adapter-pg';
 
-import {PrismaClient} from './generated/prisma/client';
+import {PrismaClient} from './generated/prisma/client.js';
 
 function createPrismaClient(): PrismaClient {
   const adapter = new PrismaPg({connectionString: env.DATABASE_URL});
@@ -20,4 +20,4 @@ export async function disconnectPrisma(): Promise<void> {
   await prisma.$disconnect();
 }
 
-export type {PrismaClient} from './generated/prisma/client';
+export type {PrismaClient} from './generated/prisma/client.js';
