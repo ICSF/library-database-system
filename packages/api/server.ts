9656c6a48d61d3f437fe01f253a91c7b5db459a5
Shriya Sistla <ss5324@ic.ac.uk>
2026-09-18 17:25:11 +0100

    [config] fix config imports

--- packages/api/server.ts
+++ packages/api/server.ts
@@ -1,6 +1,6 @@
 // server setup for local development
 
-import {env} from '@library/config/load-env';
+import {env} from '@library/config/';
 import {disconnectPrisma} from '@library/db';
 import {createHTTPServer} from '@trpc/server/adapters/standalone';
 import cors from 'cors';
